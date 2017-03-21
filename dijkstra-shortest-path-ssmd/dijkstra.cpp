0a0e8e4879139168f39a8e7788cc436e5692992a
bokonist <jayrm1995@gmail.com>
2017-03-21 12:36:04 +0530

    update dijkstra

--- dijkstra-shortest-path-ssmd/dijkstra.cpp
+++ dijkstra-shortest-path-ssmd/dijkstra.cpp
@@ -1,6 +1,7 @@
 #include <bits/stdc++.h>
 #include <GL/glut.h>
 #include "../include/graph-graphics.h"
+using namespace std;
 
 int *color; // holds the colors of nodes
 int *ecolor; //holds the colors of edges for final shortest path display
@@ -14,52 +15,83 @@ void display(); // prototype
 vector <int> shortest_path;
 bool done=false; //to display the result if the algo is done running
 
+
+struct compare  
+ {  
+   bool operator()(const int& l, const int& r)  
+   {  
+       return l > r;  
+   }  
+ };
+priority_queue<int,vector<int>, compare > prio_queue; // min prio queue
 void init_dijkstra(int source)
 {
-	memset(color, WHITE,n*sizeof(int));
-	memset(distance, INT_MAX ,n*sizeof(int));
-	memset(predecessor , -1 , n*sizeof(int)); // -1 stands for NIL
-	distance[source] = 0;
+	/*memset(color, WHITE,n*sizeof(int));
+	memset(::distance, INT_MAX ,n*sizeof(int));
+	memset(::predecessor , -1 , n*sizeof(int)); // -1 stands for NIL*/
+	for(int i=0;i<n;i++)
+	{
+		color[i]=WHITE;
+		::distance[i]=INT_MAX;
+		::predecessor[i]=-1;
+	}
+	::distance[source] = 0;
 }
 int findnode(int d)
 {
 	for (int i = 0; i < n; ++i)
 	{
-		if(distance[i]==d)
+		if(::distance[i]==d)
 			return i;
 	}
 }
 void relax(int u, int v, int w)
 {
-	int temp = w + distance[u];
-	if(distance[v] > temp)
+	int temp = w + ::distance[u];
+	if(::distance[v] > temp)
 	{
-		distance[v] = temp;
+		::distance[v] = temp;
 		predecessor[v]=u;
 	}
 }
-void dijkstra(int src) // bfs algo
+void updatePrioQueue(bool firstcall=false)
 {
-	shortest_path.push_back(src);
-	init_dijkstra(src);
-	predecessor[src] = -1;
-	priority_queue<int, std::greater<int> > prio_queue;
+	while(!prio_queue.empty())
+		prio_queue.pop();
+	if(firstcall)
+		prio_queue.push(::distance[0]);
 	for (int i = 0; i < n; ++i)
 	{
-		prio_queue.push(distance[i]);
+		if(color[i]==WHITE)
+		{
+			//cout<<"Node "<<i<<" is white\n";
+			cout<<"Pushing "<<::distance[i]<<" to prio queue\n";
+			prio_queue.push(::distance[i]);
+		}
+		cout<<endl;
 	}
+}
+void dijkstra(int src) // dijkstra algo
+{
+	shortest_path.push_back(src);
+	predecessor[src] = -1;
+	updatePrioQueue(true);
 	while(!prio_queue.empty())
 	{
-		int u= findnode(prio_queue.pop());
-		shortest_path.insert(u);
-		for(auto a : adjlist[u])
+		int u_temp= findnode(prio_queue.top());
+		cout<<"Extracting "<<prio_queue.top()<<" from prio queue\n";
+		prio_queue.pop();
+		shortest_path.push_back(u_temp);
+		for(pair<int ,int > a : adjlist[u_temp])
 		{
-			relax( u , a.first , a.second);
-			color[u]= GRAY;
+			relax( u_temp , a.first , a.second);
+			color[u_temp]= GRAY;
+			updatePrioQueue();
 			display();
 		}
 	}
-	display();
+	done=true;
+	//display();
 }
 
 
@@ -79,6 +111,7 @@ void mouse(int button, int state, int x, int y)
     if(button == GLUT_RIGHT_BUTTON && state == GLUT_DOWN)   
     {
         cout<<"\nRunning Dijkstra\n";
+        init_dijkstra(0);
         for(int i=0;i<n;i++)
 		{
 			if(color[i]==WHITE)
@@ -89,8 +122,7 @@ void mouse(int button, int state, int x, int y)
 				dijkstra(i);
 			}
 		}
-		cout<<"\n Done. Displaying result.\n";
-
+		//done=true;
     }
 }
 void display()
@@ -109,33 +141,34 @@ void display()
 	    {
 	        for(int i=0;i<n;i++)
 	        {
-	        	vector<int> neighbours = adjlist[i];
-	        	for( int j : neighbours)
+	        	vector< pair <int ,int > > neighbours = adjlist[i];
+	        	for( pair<int ,int > j : neighbours)
 	        	{
-	        		drawEdge(coordinates[i],coordinates[j]);
+	        		drawEdge(coordinates[i],coordinates[j.first]);
 	        	}
 	        }
 	    }
 	}
 	else //dijkstra is done. now display the result
 	{
+		cout<<"\n Done. Displaying result.\n";
 		for(auto a : shortest_path)
 		{
 			color[a] = GREEN;
 		}
 		for(int i=0;i<vt;i++)
 	    {
-	        str=to_string(distance[i]);
+	        str=to_string(::distance[i]);
 	        drawCircle(color[i],coordinates[i]);
 	    }
 	    if(vt==n)
 	    {
 	        for(int i=0;i<n;i++)
 	        {
-	        	vector<int> neighbours = adjlist[i];
-	        	for( int j : neighbours)
+	        	vector< pair <int ,int > > neighbours = adjlist[i];
+	        	for( pair<int ,int > j : neighbours)
 	        	{
-	        		drawEdge(coordinates[i],coordinates[j]);
+	        		drawEdge(coordinates[i],coordinates[j.first]);
 	        	}
 	        }
 	    }	
@@ -148,7 +181,7 @@ int main(int argc, char** argv)
 	cout<<"Enter the number of nodes in the graph"; nl
 	cin>>n;
 	color =  new int[n];
-	distance = new int[n];
+	::distance = new int[n];
 	predecessor = new int[n];
 	coordinates= new pair< int ,int > [n];
 	cout<<"Enter the number of edges in the graph"; nl
@@ -166,7 +199,7 @@ int main(int argc, char** argv)
 	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
 	glutInitWindowSize(700, 700);
 	glutInitWindowPosition(100, 100);
-	glutCreateWindow("Breadth First Search");
+	glutCreateWindow("Dijkstra");
 	glutDisplayFunc(display);
 	glutMouseFunc(mouse);
 	glutReshapeFunc(handleResize);
